7a127dd307cdb293c3f0cbb7aa1c4b5d28027d33
CoderCoco <christopher.jb31@gmail.com>
2023-01-01 00:45:59 -0500

    build: Add tsconfig paths to build

--- apps/electron-ui/src/main.ts
+++ apps/electron-ui/src/main.ts
@@ -2,3 +2,7 @@ import { main } from "./app/entry";
 
 main()
   .catch(e => console.error(e));
+
+// Force tsconfig paths to be included in the application output.
+// this is needed for the render process.
+export * from 'tsconfig-paths';
