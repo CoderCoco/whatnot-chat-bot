2e7e7ea797c600c54160a3b482183902d6334fe8
CoderCoco <christopher.jb31@gmail.com>
2023-01-28 14:21:52 -0500

    get application to launch

--- apps/electron-ui/src/app/entry.ts
+++ apps/electron-ui/src/app/entry.ts
@@ -1,13 +1,22 @@
 import {logCliArguments} from "@app/cli";
 import {mixitup} from "@app/mixitup-interface";
 import { app, Menu } from "electron";
+import * as process from "process";
 import { addLoggingHandler } from "./handle-logging";
 import {UserInterface} from "./user-interface";
 
 export async function main() {
+
   await app.whenReady();
 
+  if (require('electron-squirrel-startup')) {
+    app.quit();
+    return
+  }
+
   Menu.setApplicationMenu(null);
+
+  console.log('process.env.FORCE_COLOR=', process.env['FORCE_COLOR'])
 
   await addLoggingHandler();
   logCliArguments();
--- libs/core/src/lib/get-file-relative-to-dist.ts
+++ libs/core/src/lib/get-file-relative-to-dist.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 const COMPILED_DIST_ROOT = path.join(__dirname, "../../");
 
 export function getFileRelativeToDist(file: string): string {
-  logger.debug(`Resolving Path ${chalk.magentaBright(file)}`, { service: 'getFileRelativeToDist' });
+  logger.silly(`Resolving Path ${chalk.magentaBright(file)} relative to ${chalk.yellowBright(COMPILED_DIST_ROOT)}`, { service: 'getFileRelativeToDist' });
   const filePath = path.join(COMPILED_DIST_ROOT, file);
   logger.debug(`Evaluated path: ${chalk.cyanBright(filePath)}`,{ service: 'getFileRelativeToDist' });
 
